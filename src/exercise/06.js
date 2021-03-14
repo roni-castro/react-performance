16037107329d9489d755e006a8db8cd0b3f25ac1
Roni <roni.c.castro@gmail.com>
2021-03-14 20:52:51 -0300

    feat: create midle component to just render on cell value change

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -107,8 +107,16 @@ Grid = React.memo(Grid)
 function Cell({row, column}) {
   const state = useAppState()
   const cell = state.grid[row][column]
+  return <CellImp cell={cell} row={row} column={column} />
+}
+Cell = React.memo(Cell)
+
+function CellImp({cell, row, column}) {
   const dispatch = useAppDispatch()
-  const handleClick = () => dispatch({type: 'UPDATE_GRID_CELL', row, column})
+  const handleClick = React.useCallback(
+    () => dispatch({type: 'UPDATE_GRID_CELL', row, column}),
+    [dispatch, row, column],
+  )
   return (
     <button
       className="cell"
@@ -122,7 +130,7 @@ function Cell({row, column}) {
     </button>
   )
 }
-Cell = React.memo(Cell)
+CellImp = React.memo(CellImp)
 
 function DogNameInput() {
   const [state, dispatch] = useDogState()
