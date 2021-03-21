ed070dffb51e90664505dcaa3103ded66d7a1386
Roni <roni.c.castro@gmail.com>
2021-03-20 23:02:37 -0300

    feat: add trace to increment counter event

--- src/exercise/07.js
+++ src/exercise/07.js
@@ -3,10 +3,15 @@
 
 import * as React from 'react'
 import reportProfile from '../report-profile'
+import {unstable_trace as trace} from 'scheduler/tracing'
 
 function Counter() {
   const [count, setCount] = React.useState(0)
-  const increment = () => setCount(c => c + 1)
+  const increment = () => {
+    trace('counter increment', performance.now(), () => {
+      setCount(c => c + 1)
+    })
+  }
   return <button onClick={increment}>{count}</button>
 }
 
